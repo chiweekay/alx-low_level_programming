dda11d7c377d84324d64ecbd268795fc5f67c2d6
Michael Simon <simplymsi@gmail.com>
2022-12-13 07:11:40 -0800

    My First Code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,8 @@
 #include <stdio.h>
 
-/**
-
- * main - Entry point
-
- *
-
- * Return: always 0
-
- */
-
 int main(void)
   
 {
-  
-  puts("\"Programming is like building a multilingual puzzle");
-  
+  puts("Programming is like building a multilingual puzzle.\n"); 
   return (0);
-  
 }

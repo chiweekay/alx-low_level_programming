deab13590db549443b524035f492910e4c22abe3
Michael Simon <simplymsi@gmail.com>
2022-12-14 17:03:37 -0800

    Advance Task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,8 +5,7 @@
 */
 void print_times_table(int n)
 {
-int i;
-int j;
+int i; int j;
 if (n <= 15 && n >= 0)
 {
 for (i = 0; i <= n; i++)
@@ -20,16 +19,13 @@ _putchar('0');
 }
 else if (prod <= 9)
 {
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
+_putchar(','); _putchar(' ');
+_putchar(' '); _putchar(' ');
 _putchar('0' + prod);
 }
 else if (prod > 99)
 {
-_putchar(',');
-_putchar(' ');
+_putchar(','); _putchar(' ');
 _putchar('0' + (prod / 100));
 _putchar('0' + ((prod / 10) % 10));
 _putchar('0' + (prod % 10));
@@ -41,9 +37,6 @@ _putchar(' ');
 _putchar(' ');
 _putchar('0' + (prod / 10));
 _putchar('0' + (prod % 10));
-}
-}
+} }
 _putchar('\n');
-}
-}
-}
+} } }

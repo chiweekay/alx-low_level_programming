9b9da70d05b896bb31343f9107dbf083efcf37eb
Michael Simon <simplymsi@gmail.com>
2022-12-14 16:22:16 -0800

    Addition of 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* add - adding it up
+* @y: first integer
+* @z: second integer
+* Return: sum of both integers
+*/
+int add(int y, int z)
+{
+return (y + z);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -52,13 +52,15 @@ int print_last_digit(int);
  */
 void jack_bauer(void);
 /**
-
  * times_table - times this up
-
  *
-
  *
-
  */
-
 void times_table(void);
+/**
+ * add - adds stuff up
+ *
+ * @int: integers to add
+ * Return: returns a sum
+ */
+int add(int, int);
